98cefa30e1b576733d586ec080a8e5690562436d
Muhammad Makarim <mmakarim9@gmail.com>
2024-10-07 19:10:57 +0700

    Main(splash), Monitoring

--- app/src/main/java/com/example/digest/MainActivity.kt
+++ app/src/main/java/com/example/digest/MainActivity.kt
@@ -1,45 +1,25 @@
 package com.example.digest
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.digest.ui.theme.DigestTheme
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,190 +27,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DigestTheme {
-                DigestSensePreview()
+                DigSplashPreview()
             }
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
-}
 
-@Composable
-fun DigestSense() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White)
-    ) {
-
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.7f)
-                .align(Alignment.BottomCenter),
-            color = Color(0XFF080067),
-            shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
+    @Composable
+    fun DigSplash() {
+        Box(
+            modifier = Modifier.fillMaxSize()
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.splashscreen_bgn),
+                contentDescription = "Background Image",
+                modifier = Modifier.fillMaxSize()
+            )
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 16.dp, vertical = 30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-
-                Spacer(modifier = Modifier.height(100.dp))
-
-                Text(
-                    text = "Log In",
-                    fontSize = 22.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold
-                )
-
-                Spacer(modifier = Modifier.height(24.dp))
-
-                val textFieldHeight = 56.dp
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp)
-                ) {
-                    // Email TextField
-                    TextField(
-                        value = "",
-                        onValueChange = {},
-                        label = { Text("Email") },
-                        placeholder = { Text("Masukkan email") },
-                        colors = TextFieldDefaults.colors(
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent
-                        ),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(textFieldHeight)
-                            .padding(vertical = 8.dp),
-                        shape = RoundedCornerShape(8.dp)
-                    )
-
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    TextField(
-                        value = "",
-                        onValueChange = {},
-                        label = { Text("Password") },
-                        placeholder = { Text("Masukkan password") },
-                        visualTransformation = PasswordVisualTransformation(),
-                        colors = TextFieldDefaults.colors(
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent
-                        ),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(textFieldHeight)
-                            .padding(vertical = 8.dp),
-                        shape = RoundedCornerShape(8.dp)
-                    )
-
-                    Spacer(modifier = Modifier.height(32.dp))
-                    // Login Button
-                    Button(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        onClick = {},
-                        colors = ButtonDefaults.buttonColors(Color.White),
-                        shape = RoundedCornerShape(50.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(8.dp),
-                            text = "LOGIN",
-                            fontSize = 18.sp,
-                            color = Color(0XFF50279B)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.height(32.dp))
-                }
-
-                Text(
-                    textAlign = TextAlign.Center,
-                    text = "Forgot Password?",
-                    fontSize = 18.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold
-                )
-
-                Spacer(modifier = Modifier.height(24.dp))
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    // Google Button
-                }
-            }
-        }
-
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.4f)
-                .align(Alignment.TopCenter),
-            shape = RoundedCornerShape(bottomStart = 70.dp, bottomEnd = 70.dp),
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.radialGradient(
-                            colors = listOf(Color(0xFF531955), Color(0xFF11DCE8)),
-                            center = Offset(10.5f, 10.5f),
-                            radius = 1000f
-                        )
-                    )
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.padding(10.dp)
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.size(1000.dp)
                 ) {
                     Image(
-                        modifier = Modifier.size(150.dp),
+                        painter = painterResource(id = R.drawable.loader),
+                        contentDescription = "Loader Logo",
+                        modifier = Modifier.size(950.dp)
+                    )
+                    Image(
                         painter = painterResource(id = R.drawable.logo_digester),
-                        contentDescription = "picture"
-                    )
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Text(
-                        modifier = Modifier.padding(horizontal = 50.dp),
-                        text = "Digest Sense App",
-                        fontSize = 20.sp,
-                        color = Color(0XFFFFFFFF),
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center
-                    )
-
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Text(
-                        modifier = Modifier.padding(horizontal = 50.dp),
-                        text = "Log in to your account to access the app",
-                        fontSize = 15.sp,
-                        color = Color(0XFFFFFFFF),
-                        fontWeight = FontWeight.SemiBold,
-                        textAlign = TextAlign.Center
+                        contentDescription = "DigestSense Logo",
+                        modifier = Modifier.size(225.dp)
                     )
                 }
             }
         }
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DigestSensePreview() {
-    DigestTheme {
-        DigestSense()
+    @Preview(showBackground = true)
+    @Composable
+    fun DigSplashPreview() {
+        DigestTheme {
+            DigSplash()
+        }
     }
 }
+
